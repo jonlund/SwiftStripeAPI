a215de43f4c514d33d334980fd46bfbe15001308
Jon Lund <jon.lund@gmail.com>
2021-04-27 23:59:17 -0600

    slashed out anything not needed

--- Tests/StripeAPITests/SwiftStripeAPITests.swift
+++ Tests/StripeAPITests/SwiftStripeAPITests.swift
@@ -6,7 +6,7 @@ final class SwiftStripeAPITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftStripeAPI().text, "Hello, World!")
+       // XCTAssertEqual(SwiftStripeAPI().text, "Hello, World!")
     }
 
     static var allTests = [
