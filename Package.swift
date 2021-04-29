6da3f7b0732d915d6d54666e70a33d409318aaf9
Jon Lund <jon.lund@gmail.com>
2021-04-29 17:08:56 -0600

    Renamed to SwiftStripeAPI

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "StripeAPI",
-            targets: ["StripeAPI"]),
+            name: "SwiftStripeAPI",
+            targets: ["SwiftStripeAPI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "StripeAPI",
+            name: "SwiftStripeAPI",
             dependencies: []),
         .testTarget(
             name: "StripeAPITests",
-            dependencies: ["StripeAPI"]),
+            dependencies: ["SwiftStripeAPI"]),
     ]
 )
